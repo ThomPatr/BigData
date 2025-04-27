4d768bb2243015495ff2d36ad7b308f996fcc93f
MariarosaMarchese <mariarosamarchese2@gmail.com>
2025-04-27 11:09:21 +0200

    adding flags for pca and cross-validation selection

--- classification.py
+++ classification.py
@@ -22,41 +22,49 @@ class Classification():
         print("\n1. RANDOM FOREST CLASSIFICATION")
         assembler = VectorAssembler(inputCols=final_features, outputCol="features_assembled")
         scaler = StandardScaler(inputCol="features_assembled", outputCol="features_scaled")
-        pca = PCA(k=5, inputCol="features_scaled", outputCol="pca_features") # Reduce the dimensionality of the features to 5 components
 
-        rf = RandomForestClassifier(featuresCol="pca_features", labelCol="label_indexed")
+        if config.pca_bool:
+            print("Utilizziamo la PCA.")
+            pca = PCA(k=5, inputCol="features_scaled", outputCol="pca_features") # Reduce the dimensionality of the features to 5 components
+            rf = RandomForestClassifier(featuresCol="pca_features", labelCol="label_indexed")
+            pipeline_rf = Pipeline(stages=[assembler, scaler, pca, rf])
+        else:
+            print("Non utilizziamo la PCA.")
+            rf = RandomForestClassifier(featuresCol="features_scaled", labelCol="label_indexed")
+            pipeline_rf = Pipeline(stages=[assembler, scaler, rf])
 
-        paramGrid_rf_pca = (ParamGridBuilder()
-                            .addGrid(rf.numTrees, [10, 50])
-                            .addGrid(rf.maxDepth, [5, 10])
-                            .build())
+        paramGrid_rf = (ParamGridBuilder()
+                        .addGrid(rf.numTrees, [10, 50])
+                        .addGrid(rf.maxDepth, [5, 10])
+                        .build())
         
-        evaluator_rf_pca = MulticlassClassificationEvaluator(labelCol="label_indexed", predictionCol="prediction", metricName="accuracy")
+        evaluator_rf = MulticlassClassificationEvaluator(labelCol="label_indexed", predictionCol="prediction", metricName="accuracy")
         
-        pipeline_rf_pca = Pipeline(stages=[assembler, scaler, pca, rf])
-        """ cv_rf_pca = CrossValidator(estimator=pipeline_rf_pca,
-                                   estimatorParamMaps=paramGrid_rf_pca,
-                                   evaluator=evaluator_rf_pca,
-                                   numFolds=5) # 5-fold cross-validation """
-        
-        
-        tvs_rf_pca = TrainValidationSplit(estimator=pipeline_rf_pca,
-                                         estimatorParamMaps=paramGrid_rf_pca,
-                                         evaluator=evaluator_rf_pca,
-                                         trainRatio=0.8)
+        if config.cross_validation_bool:
+            print("Utilizziamo la cross-validation.")
+            validator = CrossValidator(estimator=pipeline_rf,
+                                    estimatorParamMaps=paramGrid_rf,
+                                    evaluator=evaluator_rf,
+                                    numFolds=5) # 5-fold cross-validation
+        else:
+            print("Utilizziamo la train-validation split.")
+            validator = TrainValidationSplit(estimator=pipeline_rf,
+                                            estimatorParamMaps=paramGrid_rf,
+                                            evaluator=evaluator_rf,
+                                            trainRatio=0.8)
 
-        model = tvs_rf_pca.fit(train)
+        model = validator.fit(train)
         predictions = model.transform(test)
 
-        accuracy = evaluator_rf_pca.evaluate(predictions)
+        accuracy = evaluator_rf.evaluate(predictions)
         precision = MulticlassClassificationEvaluator(labelCol="label_indexed", predictionCol="prediction", metricName="weightedPrecision").evaluate(predictions)
         recall = MulticlassClassificationEvaluator(labelCol="label_indexed", predictionCol="prediction", metricName="weightedRecall").evaluate(predictions)
         f1 = MulticlassClassificationEvaluator(labelCol="label_indexed", predictionCol="prediction", metricName="f1").evaluate(predictions)
 
-        print(f"Random Forest with PCA Accuracy: {accuracy:.2f}")
-        print(f"Random Forest with PCA Precision: {precision:.2f}")
-        print(f"Random Forest with PCA Recall: {recall:.2f}")
-        print(f"Random Forest with PCA F1 Score: {f1:.2f}")
+        print(f"Random Forest Accuracy: {accuracy:.2f}")
+        print(f"Random Forest Precision: {precision:.2f}")
+        print(f"Random Forest Recall: {recall:.2f}")
+        print(f"Random Forest F1 Score: {f1:.2f}")
 
         # Let's represent the confusion matrix
         predictions_pd = predictions.toPandas()
@@ -82,36 +90,43 @@ class Classification():
         print("\n2. DECISION TREE CLASSIFICATION")
         assembler = VectorAssembler(inputCols=final_features, outputCol="features_assembled")
         scaler = StandardScaler(inputCol="features_assembled", outputCol="features_scaled")
-        pca = PCA(k=5, inputCol="features_scaled", outputCol="pca_features")
 
-        dt = DecisionTreeClassifier(featuresCol="pca_features", labelCol="label_indexed")
+        if config.pca_bool:
+            pca = PCA(k=5, inputCol="features_scaled", outputCol="pca_features")
+            dt = DecisionTreeClassifier(featuresCol="pca_features", labelCol="label_indexed")
+            pipeline_dt = Pipeline(stages=[assembler, scaler, pca, dt])
+        else:
+            dt = DecisionTreeClassifier(featuresCol="features_scaled", labelCol="label_indexed")
+            pipeline_dt = Pipeline(stages=[assembler, scaler, dt])
 
-        paramGrid_dt_pca = (ParamGridBuilder()
-                            .addGrid(dt.maxDepth, [5, 10])
-                            .build())
+        paramGrid_dt = (ParamGridBuilder()
+                        .addGrid(dt.maxDepth, [5, 10])
+                        .build())
         
-        evaluator_dt_pca = MulticlassClassificationEvaluator(labelCol="label_indexed", predictionCol="prediction", metricName="accuracy")
-        pipeline_dt_pca = Pipeline(stages=[assembler, scaler, pca, dt])
-        """ cv_dt_pca = CrossValidator(estimator=pipeline_dt_pca,
-                                   estimatorParamMaps=paramGrid_dt_pca,
-                                   evaluator=evaluator_dt_pca,
-                                   numFolds=5) """
-        tvs_dt_pca = TrainValidationSplit(estimator=pipeline_dt_pca,
-                                         estimatorParamMaps=paramGrid_dt_pca,
-                                         evaluator=evaluator_dt_pca,
-                                         trainRatio=0.8)
-        model = tvs_dt_pca.fit(train)
+        evaluator_dt = MulticlassClassificationEvaluator(labelCol="label_indexed", predictionCol="prediction", metricName="accuracy")
+
+        if config.cross_validation_bool:
+            validator = CrossValidator(estimator=pipeline_dt,
+                                    estimatorParamMaps=paramGrid_dt,
+                                    evaluator=evaluator_dt,
+                                    numFolds=5)
+        else:
+            validator = TrainValidationSplit(estimator=pipeline_dt,
+                                            estimatorParamMaps=paramGrid_dt,
+                                            evaluator=evaluator_dt,
+                                            trainRatio=0.8)
+        model = validator.fit(train)
         predictions = model.transform(test)
 
-        accuracy = evaluator_dt_pca.evaluate(predictions)
+        accuracy = evaluator_dt.evaluate(predictions)
         precision = MulticlassClassificationEvaluator(labelCol="label_indexed", predictionCol="prediction", metricName="weightedPrecision").evaluate(predictions)
         recall = MulticlassClassificationEvaluator(labelCol="label_indexed", predictionCol="prediction", metricName="weightedRecall").evaluate(predictions)
         f1 = MulticlassClassificationEvaluator(labelCol="label_indexed", predictionCol="prediction", metricName="f1").evaluate(predictions)
 
-        print(f"Decision Tree with PCA Accuracy: {accuracy:.2f}")
-        print(f"Decision Tree with PCA Precision: {precision:.2f}")
-        print(f"Decision Tree with PCA Recall: {recall:.2f}")
-        print(f"Decision Tree with PCA F1 Score: {f1:.2f}")
+        print(f"Decision Tree Accuracy: {accuracy:.2f}")
+        print(f"Decision Tree Precision: {precision:.2f}")
+        print(f"Decision Tree Recall: {recall:.2f}")
+        print(f"Decision Tree F1 Score: {f1:.2f}")
 
         # Let's represent the confusion matrix
         predictions_pd = predictions.toPandas()
@@ -136,47 +151,60 @@ class Classification():
         print("\n3. MULTILAYER PERCEPTRON CLASSIFICATION")
         assembler = VectorAssembler(inputCols=final_features, outputCol="features_assembled")
         scaler = StandardScaler(inputCol="features_assembled", outputCol="features_scaled")
-        pca = PCA(k=5, inputCol="features_scaled", outputCol="pca_features")
 
         num_classes = train.select("label_indexed").distinct().count()
+        if config.pca_bool:
+            pca = PCA(k=5, inputCol="features_scaled", outputCol="pca_features")
+            mlp = MultilayerPerceptronClassifier(
+                    featuresCol="pca_features",
+                    labelCol="label_indexed",
+                    layers=[5, 10, 5, num_classes],
+                    blockSize=128,
+                    maxIter=100,
+                    seed=42
+            )
+            pipeline_mlp = Pipeline(stages=[assembler, scaler, pca, mlp])
+        else:
+            mlp = MultilayerPerceptronClassifier(
+                    featuresCol="features_scaled",
+                    labelCol="label_indexed",
+                    layers=[len(final_features), 10, 5, num_classes],
+                    blockSize=128,
+                    maxIter=100,
+                    seed=42
+            )
+            pipeline_mlp = Pipeline(stages=[assembler, scaler, mlp])
 
-        mlp = MultilayerPerceptronClassifier(
-                featuresCol="pca_features",
-                labelCol="label_indexed",
-                layers=[5, 10, 5, num_classes],
-                blockSize=128,
-                maxIter=100,
-                seed=42
-        )
-
-        paramGrid_mlp_pca = (ParamGridBuilder()
+        paramGrid_mlp = (ParamGridBuilder()
                              .addGrid(mlp.maxIter, [25, 50])
                              .addGrid(mlp.blockSize, [32, 64])
                              .build())
         
-        evaluator_mlp_pca = MulticlassClassificationEvaluator(labelCol="label_indexed", predictionCol="prediction", metricName="accuracy")
-        pipeline_mlp_pca = Pipeline(stages=[assembler, scaler, pca, mlp])
-        """ cv_mlp_pca = CrossValidator(estimator=pipeline_mlp_pca,
-                                   estimatorParamMaps=paramGrid_mlp_pca,
-                                   evaluator=evaluator_mlp_pca,
-                                   numFolds=5) """
-        tvs_mlp_pca = TrainValidationSplit(estimator=pipeline_mlp_pca,
-                                         estimatorParamMaps=paramGrid_mlp_pca,
-                                         evaluator=evaluator_mlp_pca,
-                                         trainRatio=0.8)
+        evaluator_mlp = MulticlassClassificationEvaluator(labelCol="label_indexed", predictionCol="prediction", metricName="accuracy")
+
+        if config.cross_validation_bool:
+            validator = CrossValidator(estimator=pipeline_mlp,
+                                    estimatorParamMaps=paramGrid_mlp,
+                                    evaluator=evaluator_mlp,
+                                    numFolds=5)
+        else:
+            validator = TrainValidationSplit(estimator=pipeline_mlp,
+                                            estimatorParamMaps=paramGrid_mlp,
+                                            evaluator=evaluator_mlp,
+                                            trainRatio=0.8)
         
-        model = tvs_mlp_pca.fit(train)
+        model = validator.fit(train)
         predictions = model.transform(test)
 
-        accuracy = evaluator_mlp_pca.evaluate(predictions)
+        accuracy = evaluator_mlp.evaluate(predictions)
         precision = MulticlassClassificationEvaluator(labelCol="label_indexed", predictionCol="prediction", metricName="weightedPrecision").evaluate(predictions)
         recall = MulticlassClassificationEvaluator(labelCol="label_indexed", predictionCol="prediction", metricName="weightedRecall").evaluate(predictions)
         f1 = MulticlassClassificationEvaluator(labelCol="label_indexed", predictionCol="prediction", metricName="f1").evaluate(predictions)
 
-        print(f"Multilayer Perceptron with PCA Accuracy: {accuracy:.2f}")
-        print(f"Multilayer Perceptron with PCA Precision: {precision:.2f}")
-        print(f"Multilayer Perceptron with PCA Recall: {recall:.2f}")
-        print(f"Multilayer Perceptron with PCA F1 Score: {f1:.2f}")
+        print(f"Multilayer Perceptron Accuracy: {accuracy:.2f}")
+        print(f"Multilayer Perceptron Precision: {precision:.2f}")
+        print(f"Multilayer Perceptron Recall: {recall:.2f}")
+        print(f"Multilayer Perceptron F1 Score: {f1:.2f}")
 
         # Let's represent the confusion matrix
         predictions_pd = predictions.toPandas()
